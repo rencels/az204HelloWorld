6534c0c26a972e77ddb04e0f6af74a699eb9b045
SebastjanRencel <rencels@gmail.com>
2026-01-06 11:53:11 +0100

    Configure forwarded headers and update pipeline

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,14 +1,39 @@
+using HelloWorld.Components;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
 var app = builder.Build();
 
-app.UseForwardedHeaders(forwardedHeadersOptions);
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor |
+        ForwardedHeaders.XForwardedProto |
+        ForwardedHeaders.XForwardedHost
+});
 
-// ❌ NO HTTPS REDIRECTION
-// app.UseHttpsRedirection();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+// REMOVE or comment this out
+ //app.UseHttpsRedirection();
 
-app.UseStaticFiles();
+app.Urls.Add("http://0.0.0.0:8080");
+
 app.UseAntiforgery();
 
+app.MapStaticAssets();
 app.MapRazorComponents<App>()
-   .AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode();
 
-app.Run();
+app.Run(); 
